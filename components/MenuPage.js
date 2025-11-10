7ae5bd413c0c1dd099ef2f973a28494ef1114ec7
harishummam <harishummambaluch@gmail.com>
2025-11-10 15:03:29 +0500

    import styles for the Menu component
    We create a style element using the document.createElement and append it to the shadow DOM. Then we create a loadCss function in the constructor where we fetch the css data from the MenuPage.css external file and load it into the textContent of the styles document we created

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,8 +1,18 @@
 export class MenuPage extends HTMLElement {
   constructor() {
     super();
-
     this.root = this.attachShadow({ mode: "open" });
+
+    const styles = document.createElement("style");
+    this.root.appendChild(styles);
+
+    async function loadCSS() {
+      const request = await fetch("/components/MenuPage.css");
+      const css = await request.text();
+      styles.textContent = css;
+    }
+
+    loadCSS();
   }
 
   // When the component gets attached to the DOM
